9cdb3281e6bb12b27b88111c148eb349a002c84b
Louis Gresta <louis.gresta@gmail.com>
2023-05-08 21:59:43 +0200

    add buttons

--- canevas.cpp
+++ canevas.cpp
@@ -18,4 +18,10 @@ void Canevas::paintEvent(QPaintEvent *event)
     QPainter painter(this);
     QImage background(":/ressources/background/winter_battle_background.jpg");
     painter.drawImage(0, 0, background);
+    QPen pen = QPen(QColor(100, 100, 100));
+    painter.setBrush(QBrush(QColor(240, 240, 240)));
+    int widthRect = 6;
+    pen.setWidth(widthRect);
+    painter.setPen(pen);
+    painter.drawRect(widthRect/2, 414+widthRect/2, 621-widthRect, 100-widthRect);
 }
