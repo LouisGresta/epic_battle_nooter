31fd8f9b772844beef17f2c5e090864f35a54fcd
Louis Gresta <louis.gresta@gmail.com>
2023-05-09 19:19:45 +0200

    travaux graphiques

--- canevas.h
+++ canevas.h
@@ -1,11 +1,31 @@
 #ifndef CANEVAS_H
 #define CANEVAS_H
 
+#include <QGridLayout>
+#include <QLabel>
 #include <QWidget>
 
 class Canevas : public QWidget
 {
     Q_OBJECT
+private:
+    QGridLayout *grid_layout = new QGridLayout();
+    QLabel message = QLabel("Je ne suis pas un hero, je veux pas croire \nce que disent les journaux");
+    QLabel cases[2][2] = {
+        {
+            QLabel("first case"),
+            QLabel("second case")
+        },
+        {
+            QLabel("third case"),
+            QLabel("fourth case")
+        }
+    };
+    int selected_case_indexes[2] = {0, 0};
+    QLabel *selected_case = &cases[selected_case_indexes[0]][selected_case_indexes[1]];
+
+    void updateSelectedCase();
+
 public:
     explicit Canevas(QWidget *parent = nullptr);
 
